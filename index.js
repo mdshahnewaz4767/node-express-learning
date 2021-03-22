688aeeef0ebb80098656dcfe9110bff220281788
shahnewaz601 <shahnewaz601@gmail.com>
2021-03-23 00:30:02 +0600

    start a server

--- index.js
+++ index.js
@@ -2,12 +2,8 @@ const express = require('express');
 
 const app = express();
 
-// const rootCall = (req, res) => {
-//     res.send('Thank you very much')
-// }
-
 app.get('/', (req, res) => {
-    res.send('Thank you very much')
+    res.send("I know how to open node");
 })
 
 app.listen(3000, () => console.log("Listening to port 3000"))
